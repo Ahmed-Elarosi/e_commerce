633d1d2f68f96f1d755253b4237c0eac5f7aa6d9
Ahmed-Elarosi <cabry95@protonmail.com>
2024-09-15 13:30:15 +0200

    add api end points

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,55 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req,res)=>{
-    res.send("Server is ready");
-})
+app.use(express.json());
 
-app.listen(5000, ()=> {
-    connectDB();
-    console.log("Server started at http://localhost:5000")
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "products not found" });
+  }
 });
 
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // sending data from user
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in creating product:", error.message);
+    res.status(500).json({ success: false, message: "server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: `Product deleted` });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});

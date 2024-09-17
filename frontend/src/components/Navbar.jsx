24067cce4400fc4f9096f5b830d342193beab7ff
Ahmed-Elarosi <cabry95@protonmail.com>
2024-09-17 16:52:30 +0200

    add navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
-//import React from 'react'
+import { Container, Flex, Text, Link } from "@chakra-ui/react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgClip="text"
+          fontWeight="bold"
+        >
+          <Link to={"/"}>Product Store</Link>
+        </Text>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
